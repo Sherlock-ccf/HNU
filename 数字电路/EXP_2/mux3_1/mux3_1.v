57cae7ce5b982860c1b039693c30b92655dac6e0
Sherlock-ccf <2463193046@qq.com>
2026-04-05 10:15:04 +0800

    Add files via upload

---
+++ 数字电路/EXP_2/mux3_1/mux3_1.v
@@ -0,0 +1,15 @@
+module mux3_1(a,b,c,s,y);
+    input [7:0] a,b,c;
+    input [1:0] s;
+    output [7:0] y;
+    reg [7:0] y;
+    always @(*)
+    begin
+      case (s)
+      2'b00: y=a;
+      2'b01: y=b;
+      2'b10: y=c;
+      default:y=a;
+      endcase
+    end
+endmodule
